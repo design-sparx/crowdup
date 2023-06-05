3d9d6012f17af6f0ef9dd34dcd732645550bf19d
Kelvin Kiptum Kiprop <kelvin.kiprop96@gmail.com>
2023-06-05 13:45:02 +0300

    updated react router dom logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,14 @@ import {customTheme} from "./theme";
 import MontserratFont from "./fonts/MontserratFont";
 
 import './App.css'
+import {RouterProvider} from "react-router-dom";
+import router from "./routes";
 
 function App() {
     return (
         <>
             <MantineProvider theme={customTheme}>
+                <RouterProvider router={router}/>
                 <MontserratFont/>
                 <Notifications/>
             </MantineProvider>
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {RouterProvider} from "react-router-dom";
-import router from "./routes";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <RouterProvider router={router}/>
         <App/>
     </React.StrictMode>,
 )
