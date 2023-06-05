7b49ca82527f94d54f3e173853998e62090fbe34
Kelvin Kiptum Kiprop <kelvin.kiprop96@gmail.com>
2023-06-05 12:57:39 +0300

    updated react router dom logic

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {RouterProvider} from "react-router-dom";
-import router from "./routes";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
